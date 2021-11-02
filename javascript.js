14d9a862564f56d05ada64336b0952fd845f41ed
nippunippu <nippunippu@protonmail.com>
2021-11-02 15:06:26 -0300

    rps-ui

--- javascript.js
+++ javascript.js
@@ -1,82 +1,86 @@
 let player_score = 0
 let computer_score = 0
 let announce = ""
+let final_announce = ""
 let round = []
 
 let plays = ['rock', 'paper', 'scissors']
-
-let computerPlay = () => {
-	return plays[Math.floor(Math.random() * 3)]
-};
-
-let playerPlay = () => {
-	let player_play = prompt("Type your play").toLowerCase()
-	return player_play
-}
 
 let playRound = (playerSelection, computerSelection) => {
 
 	if (playerSelection == "rock") {
 		if (computerSelection == "scissors") {
 			player_score = player_score + 1
-			announce = "Player's " + playerSelection + " wins Computer's " + computerSelection
+			announce = "You WIN! PLAYER: " + playerSelection + " WINS CPU: " + computerSelection
 		}
 		if (computerSelection == "paper") {
 			computer_score = computer_score + 1
-			announce = "Player's " + playerSelection + " loses to Computer's " + computerSelection
+			announce = "You LOSE! CPU: " + computerSelection + " WINS PLAYER: " + playerSelection
 		}
 		if (computerSelection == "rock") {
-			announce = "Player's " + playerSelection + " ties to Computer's " + computerSelection
+			announce = "TIE! PLAYER: " + playerSelection + " CPU: " + computerSelection
 		}
 	}
 	if (playerSelection == "scissors") {
 		if (computerSelection == "paper") {
 			player_score = player_score + 1
-			announce = "Player's " + playerSelection + " wins Computer's " + computerSelection
+			announce = "You WIN! PLAYER: " + playerSelection + " WINS CPU: " + computerSelection
 		}
 		if (computerSelection == "rock") {
 			computer_score = computer_score + 1
-			announce = "Player's " + playerSelection + " loses to Computer's " + computerSelection
+			announce = "You LOSE! CPU: " + computerSelection + " WINS PLAYER: " + playerSelection
 		}
 		if (computerSelection == "scissors") {
-			announce = "Player's " + playerSelection + " ties to Computer's " + computerSelection
+			announce = "TIE! PLAYER: " + playerSelection + " CPU: " + computerSelection
 		}
 	}
 	if (playerSelection == "paper") {
 		if (computerSelection == "rock") {
 			player_score = player_score + 1
-			announce = "Player's " + playerSelection + " wins Computer's " + computerSelection
+			announce = "You WIN! PLAYER: " + playerSelection + " WINS CPU: " + computerSelection
 		}
 		if (computerSelection == "scissors") {
 			computer_score = computer_score + 1
-			announce = "Player's " + playerSelection + " loses to Computer's " + computerSelection
+			announce = "You LOSE! CPU: " + computerSelection + " WINS PLAYER: " + playerSelection
 		}
 		if (computerSelection == "paper") {
-			announce = "Player's " + playerSelection + " ties to Computer's " + computerSelection
+			announce = "TIE! PLAYER: " + playerSelection + " CPU: " + computerSelection
 		}
 	}
 
-	console.log(announce)
+	if (player_score > 4) {
+		final_announce = "PLAYER WINS " + player_score + " TO CPU " + computer_score
+	}
 
-	return [player_score, computer_score, announce]
+	if (computer_score > 4) {
+		final_announce = "PLAYER LOSES " + player_score + " TO CPU " + computer_score
+	}
+
+	return [player_score, computer_score, announce, final_announce]
+
 }
 
-let game = () => {
-	for (i = 1; i < 6;i++) {
-		round = playRound(playerPlay(), computerPlay())
-		console.log(round[0], round[1])
-	}
-	if (round[0] > round[1]) {
-		return "Player WIN: " + round[0] + " Computer LOSE: " + round[1]
-	}
-	if (round[0] < round[1]) {
-		return "Player LOSE: " + round[0] + " Computer WIN: " + round[1]
-	}
+let computerPlay = () => {
+	return plays[Math.floor(Math.random() * 3)]
+};
 
-	if (round[0] == round[1]) {
-		return "Player TIE: " + round[0] + " Computer TIE: " + round[1]
-	}
-}
+const rock_button = document.querySelector('#rock_button')
+const paper_button = document.querySelector('#paper_button')
+const scissors_button = document.querySelector('#scissors_button')
 
+const display_results = document.querySelector('#display_results')
 
-console.log(game())
+rock_button.addEventListener('click', function () {
+	round = playRound('rock', computerPlay())
+	display_results.innerHTML = round
+})
+
+paper_button.addEventListener('click', function () {
+	round = playRound('paper', computerPlay())
+	display_results.innerHTML = round
+})
+
+scissors_button.addEventListener('click', function () {
+	round = playRound('scissors', computerPlay())
+	display_results.innerHTML = round
+})
